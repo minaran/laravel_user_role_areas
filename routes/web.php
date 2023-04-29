888ac3b1055ab29b8be2d3044b9b0ae49e6286f9
Mina Rancic <minarancic@gmail.com>
2023-04-29 15:17:16 +0200

    middleware i prefix za vise grupa u okviru grupe

--- routes/web.php
+++ routes/web.php
@@ -18,14 +18,31 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/student/timetable', [\App\Http\Controllers\Student\TimetableController::class, 'index']) 
-    ->middleware(['auth', 'verified'])
-    ->name('student.timetable');
+// ako imamo vise korisnika grupisacemo ih da se ne ponavlja kod
+Route::middleware(['auth', 'verified'])->group(function() {
 
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->middleware(['auth', 'verified'])->name('dashboard');
+    
+    // ovim smo napravili i grupu ako kod studenat budemo imali vise tpova studenata
+    Route::prefix('student')
+    ->name('student.')
+    ->group( function(){
+
+    // prvobitno smo ovu smo rutu kopirali kao i za dashboard i prilagodili za studenta
+    
+        // Route::get('/student/timetable', [\App\Http\Controllers\Student\TimetableController::class, 'index'])   // prvi red ostavljamo malo izmenjeno
+        // ->middleware(['auth', 'verified']) // ni ovo nam netreba jer imamo gore grupisano red 23
+        //->name('student.timetable');  // napisacemo jednostavnije drugacije:
+        Route::get('timetable', [\App\Http\Controllers\Student\TimetableController::class, 'index']) 
+        ->name('timetable');
+    
+    // Route::get('').....tu bi stavili drugu rutu na primer za studenta2..... 
+
+    });
+});
 
 
 Route::middleware('auth')->group(function () {
